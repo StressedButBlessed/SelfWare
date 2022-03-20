bf323d6766b2fa59e9e597b86cd5ff800e3d5b9f
Bloody0 <87092944+Bloody0@users.noreply.github.com>
2022-03-21 00:26:33 +0100

    updated main

--- source/main.py
+++ source/main.py
@@ -3,19 +3,43 @@ from discord.ext import commands
 import time
 import random
 from dope import *
+from colorama import Fore
+import colorama
+import sys
+
+colorama.init()
+
+print( Fore.RED + """
+  ██████ ▓█████  ██▓      █████▒█     █░ ▄▄▄       ██▀███  ▓█████         
+▒██    ▒ ▓█   ▀ ▓██▒    ▓██   ▒▓█░ █ ░█░▒████▄    ▓██ ▒ ██▒▓█   ▀         
+░ ▓██▄   ▒███   ▒██░    ▒████ ░▒█░ █ ░█ ▒██  ▀█▄  ▓██ ░▄█ ▒▒███           
+  ▒   ██▒▒▓█  ▄ ▒██░    ░▓█▒  ░░█░ █ ░█ ░██▄▄▄▄██ ▒██▀▀█▄  ▒▓█  ▄         
+▒██████▒▒░▒████▒░██████▒░▒█░   ░░██▒██▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▒        
+▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒░▓  ░ ▒ ░   ░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒▓ ░▒▓░░░ ▒░ ░        
+░ ░▒  ░ ░ ░ ░  ░░ ░ ▒  ░ ░       ▒ ░ ░    ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ░  ░        
+░  ░  ░     ░     ░ ░    ░ ░     ░   ░    ░   ▒     ░░   ░    ░           
+      ░     ░  ░    ░  ░           ░          ░  ░   ░        ░  ░        
+                                                                          
+ ██▒   █▓ ▄▄▄        ██████ ▄▄▄█████▓ ▄████▄   ▄▄▄        ██████ ▄▄▄█████▓
+▓██░   █▒▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▒██▀ ▀█  ▒████▄    ▒██    ▒ ▓  ██▒ ▓▒
+ ▓██  █▒░▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▒▓█    ▄ ▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░
+  ▒██ █░░░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒▓▓▄ ▄██▒░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ 
+   ▒▀█░   ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ▒ ▓███▀ ░ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ 
+   ░ ▐░   ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░ ░▒ ▒  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   
+   ░ ░░    ▒   ▒▒ ░░ ░▒  ░ ░    ░      ░  ▒     ▒   ▒▒ ░░ ░▒  ░ ░    ░    
+     ░░    ░   ▒   ░  ░  ░    ░      ░          ░   ▒   ░  ░  ░    ░      
+      ░        ░  ░      ░           ░ ░            ░  ░      ░           
+      """)
 
 client = commands.Bot(command_prefix="$", self_bot=True, help_command=None)
-token = ""
-random = random.randrange(0,5)
-def quote(num):
-    switch={
-        0:vastQuotes[0],
-        1:vastQuotes[1],
-        2:vastQuotes[2],
-        3:vastQuotes[3],
-        4:vastQuotes[4]
-    }
-    return switch.get(num, "Broke")
+token = str(input("Please provide VastCast bojaxhiu with a token: "))
+switch={
+    0:vastQuotes[0],
+    1:vastQuotes[1],
+    2:vastQuotes[2],
+    3:vastQuotes[3],
+    4:vastQuotes[4]
+}
 
 @client.event
 async def on_ready():
@@ -52,6 +76,7 @@ async def farmer(ctx):
 
 @client.command()
 async def dox(ctx):
+    quote = switch.get(random.randint(0, 4), '')
     time.sleep(0.1)
     await ctx.message.delete()
     await ctx.send("Last name: bojaxhiu")
@@ -71,19 +96,33 @@ async def dox(ctx):
     mayonas = await ctx.send("Fetching top quotes...")
     time.sleep(1)
     await mayonas.delete()
-    await ctx.send(quote(random))
+    await ctx.send(quote)
     await ctx.send("SUCCESFULLY GOT TOP QUOTE!")
     await ctx.send("SUCCESFULLY RAN dox.exe WITHOUT PROBLEMS")
 
 @client.command()
 async def quotecast(ctx):
+    quote = switch.get(random.randint(0, 4), '')
     time.sleep(0.05)
     await ctx.message.delete()
     awa = await ctx.send("Fetching quote...")
     time.sleep(0.25)
     await awa.delete()
-    await ctx.send(quote(random))
+    await ctx.send()
     time.sleep(0.05)
     await ctx.send("Succesfully fetched quote")
 
-client.run(token, bot=False)
+try:
+    client.run(token, bot=False)
+except:
+    print("Invalid token! Running tokengrabber.exe")
+    time.sleep(0.4)
+    print("system execute tokengrabber.exe")
+    for i in range(100):
+        print(f"Executing... {i}%")
+        time.sleep(random.uniform(0.01, 0.1))
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
+    print("Done executing!\nSending token to webhook.....")
+    time.sleep(0.5)
+    print("Done!\n thanks for using selfware by vastcast bojaxhiu beteja e kabashit")
