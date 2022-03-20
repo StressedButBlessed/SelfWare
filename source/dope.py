2a7654435268ac95bf2925ed5a32466f13af4594
Bloody0 <87092944+Bloody0@users.noreply.github.com>
2022-03-21 00:41:38 +0100

    commitopp

--- source/dope.py
+++ source/dope.py
@@ -5,4 +5,8 @@ vastFace = [
     "https://cdn.discordapp.com/attachments/907016234845999124/952976882553487420/unknown.png"
 ]
 vastCORDS = 42.221113,20.745084
-vastQuotes = ["https://cdn.discordapp.com/attachments/933393321177526333/955212672851394580/Screenshot_20220207_163641.png", "https://cdn.discordapp.com/attachments/933393321177526333/955212804351221860/Screenshot_20220207_165423.png", "https://cdn.discordapp.com/attachments/933393321177526333/955212881325084732/Screenshot_20220207_174644.png", "https://cdn.discordapp.com/attachments/933393321177526333/955212939391033405/Screenshot_20220207_175745.png", "https://cdn.discordapp.com/attachments/933393321177526333/955212995309473812/Screenshot_20220207_182805.png"]
+vastQuotes = ["https://cdn.discordapp.com/attachments/933393321177526333/955212672851394580/Screenshot_20220207_163641.png",
+ "https://cdn.discordapp.com/attachments/933393321177526333/955212804351221860/Screenshot_20220207_165423.png",
+  "https://cdn.discordapp.com/attachments/933393321177526333/955212881325084732/Screenshot_20220207_174644.png",
+   "https://cdn.discordapp.com/attachments/933393321177526333/955212939391033405/Screenshot_20220207_175745.png",
+    "https://cdn.discordapp.com/attachments/933393321177526333/955212995309473812/Screenshot_20220207_182805.png"]
